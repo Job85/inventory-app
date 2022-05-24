d01315e462b602cdd4bf0d561ac6a1501fdb2839
Job85 <julianjernigan@ymail.com>
2022-05-24 15:52:23 -0400

    setup server for dev

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
+// app.use(express.static(`${__dirname}/client/build`))
+app.use(express.static())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.send({ msg: 'Server Running ' })
+})
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(`${__dirname}/client/build/index.html`)
+// })
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
