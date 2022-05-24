5232ada6db666f41d94d9b0f6c8c31966b62ac6f
Job85 <julianjernigan@ymail.com>
2022-05-24 15:59:12 -0400

    muted express build

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const app = express()
 
 app.use(cors())
 // app.use(express.static(`${__dirname}/client/build`))
-app.use(express.static())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
