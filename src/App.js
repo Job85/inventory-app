ec8d92718f401cd605d7b0bc7c93272b05530471
Job85 <julianjernigan@ymail.com>
2022-05-24 14:24:44 -0400

    created handleUpdate function and passed as props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Items from './pages/Items';
 import './App.css';
 
 function App() {
+
+  const handleUpdate = (id) => {
+    let editItem = axios.get(`${BASE_URL}/items/${id}`)
+    return editItem
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +20,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/items' element={<Items handleUpdate={handleUpdate} />} />
         </Routes>
       </main>
     </div>
