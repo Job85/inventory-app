411a1c93829b7d41cd51f38efd3724e70c5ab06c
Job85 <julianjernigan@ymail.com>
2022-05-24 15:16:37 -0400

    imported ItemDetails page and wrapped component with Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Items from './pages/Items';
+import ItemDetails from './pages/ItemDetails';
 import axios from 'axios';
 import './App.css';
 
@@ -43,6 +44,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/items' element={<Items handleUpdate={handleUpdate} />} />
+          <Route path='/items/:id' element={<ItemDetails setNewItem={setNewItem} />} />
         </Routes>
       </main>
     </div>
