bef862cac2a1898178e8a53dce6b6dee09c151b7
Job85 <julianjernigan@ymail.com>
2022-05-24 13:24:17 -0400

    created page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+
+    return (
+        <div className='home'>
+            <h1>Home Page</h1>
+            <h2>
+                Welcome to your inventory App!
+            </h2>
+        </div>
+    )
+}
+
+export default Home
