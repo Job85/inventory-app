a95679624ddbc41aa58194d23468b1900b00d83b
Job85 <julianjernigan@ymail.com>
2022-05-24 22:17:21 -0400

    updated Schema

--- server/seed/items.js
+++ server/seed/items.js
@@ -1,18 +1,17 @@
 const db = require('../db')
-const { Item, Inventory } = require('../models')
+const { Item } = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    const demoInventory = await Inventory.find({ createdAt: "2022-05-21T20:14:18.795Z" })
     const items = [
         {
             location: 'demo location',
             category: 'demo category',
-            item: 'demo item',
-            size: 'demo size',
-            count: 12,
-            inventory_id: demoInventory[0]._id
+            item_name: 'demo item',
+            description: 'demo description',
+            unit_measure: 'demo measure',
+            case_size: 'demo case size'
         }
     ]
 
