1d8faa86bc76ad9656474776b2515d2e1b159f9c
Job85 <julianjernigan@ymail.com>
2022-05-24 21:14:53 -0400

    reformated edit form to correct useEffect function and useState, corrected form to reflect item model values

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/items' element={<Items handleUpdate={handleUpdate} />} /><Route path='/new' element={<ItemForm newItem={newItem} handleChange={handleChange} handleSubmit={handleSubmit} />} />
           <Route path='/new' element={<ItemForm newItem={newItem} handleChange={handleChange} handleSubmit={handleSubmit} />} />
-          <Route path='/items/:id' element={<EditForm setNewItem={setNewItem} />} />
+          <Route path='/items/:id' element={<EditForm />} />
         </Routes>
       </main>
     </div>
--- src/pages/EditItem.js
+++ src/pages/EditItem.js
@@ -3,97 +3,154 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios'
 
 
-const EditForm = (props) => {
-    let [retrievedItem, updateItem] = useState([])
+const EditForm = () => {
+
+    const [formValues, setFormValues] = useState({
+        location: '',
+        category: '',
+        item_name: '',
+        description: '',
+        unit_measure: '',
+        case_size: ''
+    })
 
     let navigate = useNavigate()
     let { id } = useParams()
-    let getItem = async () => {
-        let pendingItem = await axios.get(`http://localhost:3001/item/${id}`)
-        return pendingItem
-    }
-    // useEffect(async () => {
-    //     let item = await getItem()
-    //     console.log(item)
-    //     updateItem(item.data[0])
-    // }, [])
+
+    useEffect(() => {
+        let isCancelled = false
+        const getItem = async () => {
+            const res = await axios.get(
+                `http://localhost:3001/item/${id}`
+            )
+            if (!isCancelled) {
+                setFormValues(res.data)
+            }
+        }
+        getItem()
+        return () => {
+            isCancelled = true
+        }
+    }, [id])
 
     const handleChange = (e) => {
-        updateItem({ ...retrievedItem, [e.target.name]: e.target.value })
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
     }
 
     const handleDelete = async (id) => {
-        await axios.delete(`http://localhost:3001/deleteitem/${id}`).then(
+        console.log('button clicked')
+        await axios.delete(`http://localhost:3001/api/item/delete/${id}`).then(
             () => navigate('/items')
         )
 
     }
 
-    const handleSave = async () => {
-        await axios.put(`http://localhost:3001/putitem/`, retrievedItem).then(
+    const updateItem = async () => {
+        let url = process.env.NODE_ENV === `http://localhost:3001/api/item/update/${id}`
+        await axios.put({
+            url,
+            method: 'put',
+            data: formValues
+        })
+    }
+    const handleSave = async (e) => {
+        console.log('button clicked')
+        e.preventDefault();
+        updateItem();
+        setFormValues({
+            location: '',
+            category: '',
+            item_name: '',
+            description: '',
+            unit_measure: '',
+            case_size: ''
+        })
+        axios.put(`http://localhost:3001/api/item/update/${id}`, formValues).then(
             () => navigate('/items')
         )
     }
 
     return (
         <div className='items'>
-            <h1>Item Details</h1>
+            <h1>Edit { }</h1>
             <div className='editContainer'>
                 <div className='edit-card'>
                     <li className='item-card'>
                         <span>
                             <label>Location:</label>
-                            <input type='text'
+                            <input
+                                value={formValues.location}
+                                type='text'
                                 onChange={handleChange}
                                 name={'location'}
-                                placeholder={`${retrievedItem.location}`}
+                                placeholder={`${formValues.location}`}
                                 className='edit-form' />
                         </span>
                     </li>
                     <li className='item-card'>
                         <span>
                             <label>Category:</label>
-                            <input type='text'
-                                onChange={props.handleChange}
+                            <input
+                                value={formValues.category}
+                                type='text'
+                                onChange={handleChange}
                                 name={'category'}
-                                placeholder={`${retrievedItem.category}`}
+                                placeholder={`${formValues.category}`}
                                 className='edit-form' />
                         </span>
                     </li>
                     <li className='item-card'>
                         <span>
                             <label>Item:</label>
-                            <input type='text'
-                                onChange={props.handleChange}
+                            <input
+                                value={formValues.item_name}
+                                type='text'
+                                onChange={handleChange}
                                 name={'item'}
-                                placeholder={`${retrievedItem.item}`}
+                                placeholder={`${formValues.item_name}`}
                                 className='edit-form' />
                         </span>
                     </li>
                     <li className='item-card'>
                         <span>
-                            <label>Size:</label>
-                            <input type='text'
-                                onChange={props.handleChange}
-                                name={'size'}
-                                placeholder={`${retrievedItem.size}`}
+                            <label>Description:</label>
+                            <input
+                                value={formValues.description}
+                                type='text'
+                                onChange={handleChange}
+                                name={'description'}
+                                placeholder={`${formValues.description}`}
                                 className='edit-form' />
                         </span>
                     </li>
                     <li className='item-card'>
                         <span>
-                            <label>Count:</label>
-                            <input type='text'
-                                onChange={props.handleChange}
-                                name={'count'}
-                                placeholder={`${retrievedItem.count}`}
+                            <label>Unit Measure:</label>
+                            <input
+                                value={formValues.unit_measure}
+                                type='text'
+                                onChange={handleChange}
+                                name={'unit-measure'}
+                                placeholder={`${formValues.unit_measure}`}
                                 className='edit-form' />
                         </span>
                     </li>
-                    <div onClick={() => handleDelete(retrievedItem._id)}>
+                    <li className='item-card'>
+                        <span>
+                            <label>Case Size:</label>
+                            <input
+                                value={formValues.case_size}
+                                type='text'
+                                onChange={handleChange}
+                                name={'case-size'}
+                                placeholder={`${formValues.case_size}`}
+                                className='edit-form' />
+                        </span>
+                    </li>
+                    <div onClick={() => handleDelete(formValues._id)}>
                         <button id='deleteButton' type='submit'>Delete</button>
                     </div>
-                    <div onClick={() => handleSave(retrievedItem)}>
+                    <div onClick={() => handleSave(formValues)}>
                         <button id='saveButton' type='submit'>Save</button>
                     </div>
                 </div>
