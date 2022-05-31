f068ba4b4a91592c01d31110c00141a79069f4f7
Job85 <julianjernigan@ymail.com>
2022-05-31 10:01:45 -0400

    changed title in index, deleted db, and unused folders

--- server/routes/InventoryRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express').Router()
-const controller = require('../controllers/InventoryController')
-
-Router.get('/', controller.getInventory)
-Router.post(`/create`, controller.postInventory)
-Router.put(`/update/:id/:departmentId`, controller.putInventory)
-Router.delete(`/delete/:id`, controller.deleteInventoryById)
-
-module.exports = Router
