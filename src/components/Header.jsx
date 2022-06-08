dc270227f9a8ecaec4ce3188067e425d3f2e47c8
Job85 <julianjernigan@ymail.com>
2022-06-08 13:54:22 -0400

    added protected links with auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,35 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({ authenticated, user, handleLogOut }) => {
+    let authOptions
+    if (user) {
+        authOptions = (
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to='new'>Create Inventory Item</Link>
+                <Link onClick={handleLogOut} to='/'>
+                    Sign Out
+                </Link>
+            </nav>
+        )
+    }
+
+    const publicOptions = (
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/register'>Sign Up for App</Link>
+            <Link to='/login'>Log In</Link>
+        </nav>
+    )
 
     return (
-        <nav className="navbar">
+        <header>
             <div>
                 <Link to='/'>Home</Link>
-                <Link to='items'>Items</Link>
-                <Link to='/new'>Add Item</Link>
+                {authenticated && user ? authOptions : publicOptions}
             </div>
-        </nav>
+        </header>
     )
 }
 
