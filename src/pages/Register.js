01f1179d9f3085fa9c152326f798be2e0503b19d
Job85 <julianjernigan@ymail.com>
2022-06-08 11:40:28 -0400

    fixed password input value

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,70 +28,70 @@ const Register = () => {
             password: '',
             confirmPassword: ''
         })
-
-        return (
-            <div className='register col'>
-                <div className='card-overlay'>
-                    <form className='col' onSubmit={handleSubmit}>
-                        <div className='input-wrapper'>
-                            <label htmlFor='username'>Username</label>
-                            <input
-                                onChange={handleChange}
-                                name='username'
-                                type="text"
-                                placeholder='Username'
-                                value={formValues.username}
-                                required
-                            />
-                        </div>
-                        <div className='input-wrapper'>
-                            <label htmlFor='username'>Email</label>
-                            <input
-                                onChange={handleChange}
-                                name='email'
-                                type="text"
-                                placeholder='example@email.com'
-                                value={formValues.email}
-                                required
-                            />
-                        </div>
-                        <div className='input-wrapper'>
-                            <label htmlFor='username'>Password</label>
-                            <input
-                                onChange={handleChange}
-                                name='password'
-                                type="password"
-                                placeholder='Username'
-                                value={formValues.username}
-                                required
-                            />
-                        </div>
-                        <div className='input-wrapper'>
-                            <label htmlFor='username'>Confirm Password</label>
-                            <input
-                                onChange={handleChange}
-                                name='confirmPassword'
-                                type="password"
-                                placeholder='Confirm Password'
-                                value={formValues.confirmPassword}
-                                required
-                            />
-                        </div>
-                        <button
-                            disabled={
-                                !formValues.username ||
-                                !formValues.email ||
-                                (!formValues.password &&
-                                    formValues.confirmPassword === formValues.password)
-                            }
-                        >
-                            Register
-                        </button>
-                    </form>
-                </div>
-            </div>
-        )
     }
+
+    return (
+        <div className='register col'>
+            <div className='card-overlay'>
+                <form className='col' onSubmit={handleSubmit}>
+                    <div className='input-wrapper'>
+                        <label htmlFor='username'>Username</label>
+                        <input
+                            onChange={handleChange}
+                            name='username'
+                            type="text"
+                            placeholder='Username'
+                            value={formValues.username}
+                            required
+                        />
+                    </div>
+                    <div className='input-wrapper'>
+                        <label htmlFor='username'>Email</label>
+                        <input
+                            onChange={handleChange}
+                            name='email'
+                            type="text"
+                            placeholder='example@email.com'
+                            value={formValues.email}
+                            required
+                        />
+                    </div>
+                    <div className='input-wrapper'>
+                        <label htmlFor='username'>Password</label>
+                        <input
+                            onChange={handleChange}
+                            name='password'
+                            type="password"
+                            placeholder='Username'
+                            value={formValues.password}
+                            required
+                        />
+                    </div>
+                    <div className='input-wrapper'>
+                        <label htmlFor='username'>Confirm Password</label>
+                        <input
+                            onChange={handleChange}
+                            name='confirmPassword'
+                            type="password"
+                            placeholder='Confirm Password'
+                            value={formValues.confirmPassword}
+                            required
+                        />
+                    </div>
+                    <button
+                        disabled={
+                            !formValues.username ||
+                            !formValues.email ||
+                            (!formValues.password &&
+                                formValues.confirmPassword === formValues.password)
+                        }
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Register
