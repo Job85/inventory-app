c75d38997c0f75fe38cdb4bc27d6c89c74bbbb38
Job85 <joulescodinglab@gmail.com>
2024-04-04 13:58:41 -0400

    added better error handling in checkSession & signInUser functions

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -3,8 +3,11 @@ import Client from "./index";
 export const signInUser = async (data) => {
     try {
         const res = await Client.post('/auth/login', data)
-        localStorage.setItem('token', res.data.token)
-        return res.data.user
+        if (res.data.token) {
+            return { user: res.data.user, token: res.data.token }
+        } else {
+            throw new Error('Token not received in response');
+        }
     } catch (error) {
         throw error
     }
@@ -21,7 +24,16 @@ export const registerUser = async (data) => {
 
 export const checkSession = async () => {
     try {
-        const res = await Client.get('/auth/session')
+        const token = localStorage.getItem('token');
+        if (!token) {
+            throw new Error('Token not found in localStorage');
+        }
+
+        const res = await Client.get('/auth/session', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
         return res.data
     } catch (error) {
         throw error
